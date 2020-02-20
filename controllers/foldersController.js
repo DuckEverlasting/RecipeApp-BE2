cdda3b4fecf53faf308e17b0417be7e8499052f6
Matt Klein <mklein246@gmail.com>
2020-02-20 13:19:34 -0800

    started work on controllers

--- controllers/foldersController.js
+++ controllers/foldersController.js
@@ -1,2 +1,36 @@
-const knex = require('../dbConfig.js');
+const generic = require("../helpers/generic.js");
 
+exports.getFolderById = (req, res) => {
+  generic
+    .getById("folders", req.params.id)
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json({ message: "Error", error: err }));
+};
+
+exports.getFoldersByUser = (req, res) => {
+  generic
+    .getBy("folders", {user: req.body.user})
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json({ message: "Error", error: err }));
+};
+
+exports.insertFolder = (req, res) => {
+  generic
+    .insert("folders", req.body.folder)
+    .then(data => res.status(201).json(data))
+    .catch(err => res.status(500).json({ message: "Error", error: err }));
+};
+
+exports.updateFolder = (req, res) => {
+  generic
+    .updateById("folders", req.body.id, req.body.changes)
+    .then(data => res.status(201).json(data))
+    .catch(err => res.status(500).json({ message: "Error", error: err }));
+};
+
+exports.deleteFolder = (req, res) => {
+  generic
+    .deleteById("folders", req.body.id)
+    .then(() => res.status(204))
+    .catch(err => res.status(500).json({ message: "Error", error: err }));
+};
